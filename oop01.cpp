1c9eca937dad5425de346cf9d78a805b4a079ee5
SarveshVeshi <veshisarvesh23@gmail.com>
2024-11-22 14:26:11 +0530

    Add files via upload

---
+++ oop01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+class bank_account
+{
+  private :
+            int acc_no , balance ;
+  public :
+            void getdata();
+            void putdata();
+           
+};
+void bank_account ::  getdata()
+            {
+                std::cout<<"\n Enter your Account number and balance :"<<std::endl;
+                std::cin>>acc_no>>balance;
+            }
+void bank_account ::  putdata()
+            {
+                std::cout<<"\n Account no :"<<acc_no<<"\n Balance :"<<balance;
+            }   
+int main()
+{
+    bank_account B;
+    B.getdata();
+    B.putdata();
+    return 0;
+}        
